e6670efe45049a9d3837f4722c281da232b95592
Jose <jrobertoortiz112@gmail.com>
2018-01-15 11:53:22 -0600

    Corrección de errores

--- baseDeDatos/servicioExcursiones.sql
+++ baseDeDatos/servicioExcursiones.sql
@@ -5,28 +5,29 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `servicioExcursiones` DEFAULT CHARACTER SET utf8 ;
 USE `servicioExcursiones` ;
-
----------------Tabla agregada en actualizacion 2--------------
+ 	
+-- -------------Tabla agregada en actualizacion 2--------------
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`categorias`(
 	`idCategoria` INT NOT NULL AUTO_INCREMENT,
-	`categoria` VARCHAR(50),
+	`categoria` VARCHAR(50) NOT NULL,
+	PRIMARY KEY(`idCategoria`)
 )
 ENGINE = InnoDB;
 
--------------------Se añade la columna descripcion--------------------
+-- -----------------Se añade la columna descripcion--------------------
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`destinosTuristicos`(
 	`idDestino` INT NOT NULL AUTO_INCREMENT,
 	`nombreDestino` VARCHAR(100) NOT NULL,
 	`idCategoria` INT NOT NULL,
 	`descripcion` VARCHAR(250) NOT NULL,
 	PRIMARY KEY(`idDestino`),
-	FOREIGN KEY(`idCategoria`) REFERENCES `destinosTuristicos`(`idCategoria`)
+	FOREIGN KEY(`idCategoria`) REFERENCES `categorias`(`idCategoria`)
 )
 ENGINE = InnoDB;
 
---Se cambia el nombre de caracteristicasDestino a comentariosDestino--
------Se cambia de nombre la columna caracteristica por comentario-----
-----Se añade la columna persona para saber quien hizo el comentario---
+-- Se cambia el nombre de caracteristicasDestino a comentariosDestino--
+-- ---Se cambia de nombre la columna caracteristica por comentario-----
+-- --Se añade la columna persona para saber quien hizo el comentario---
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`comentariosDestino`(
 	`idCaracteristica` INT NOT NULL AUTO_INCREMENT,
 	`idDestino` INT NOT NULL,
@@ -37,17 +38,17 @@ CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`comentariosDestino`(
 )
 ENGINE = InnoDB;
 
------------------Tabla agregada en actualizacion 2------------------
+-- ---------------Tabla agregada en actualizacion 2------------------
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`imagenesDestinOs`(
 	`idImagen` INT NOT NULL AUTO_INCREMENT,
 	`url` VARCHAR(100) NOT NULL,
 	`idDestino` INT NOT NULL,
-	PRIMARY KEY(),
+	PRIMARY KEY(`idImagen`),
 	FOREIGN KEY(`idDestino`) REFERENCES `destinosTuristicos`(`idDestino`)
 )
 ENGINE = InnoDB;
 
-------------------PUEDEN SER ELIMINADAS----------------------------
+-- ----------------PUEDEN SER ELIMINADAS----------------------------
 
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`marcasVehiculos`(
 	`idMarca` INT NOT NULL AUTO_INCREMENT,
@@ -58,39 +59,39 @@ ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`tipoVehiculos`(
 	`idTipoVehiculo` INT NOT NULL AUTO_INCREMENT,
-	`tipoVehiculo` VARCHAR(25),--Bus/Buseta/etc
+	`tipoVehiculo` VARCHAR(25),-- Bus/Buseta/etc
 	PRIMARY KEY(`idTipoVehiculo`)
 )
 ENGINE = InnoDB;
---------------------------------------------------------------------
+-- ------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`flotilla`(
-	`idVehiculo` VARCHAR(6) NOT NULL, --LA PLACA DE LA BUSETA/BUS
+	`idVehiculo` VARCHAR(6) NOT NULL, -- LA PLACA DE LA BUSETA/BUS
 	`idMarca` INT NOT NULL,
 	`idTipoVehiculo` INT NOT NULL,
 	`campos` INT NOT NULL,
-	`enMantenimiento` TINYINT(1), --VALOR BOOLEANO PARA SABER SI PUEDE USARSE O NO
+	`enMantenimiento` TINYINT(1), -- VALOR BOOLEANO PARA SABER SI PUEDE USARSE O NO
 	PRIMARY KEY(`idVehiculo`),
 	FOREIGN KEY(`idMarca`) REFERENCES `marcasVehiculos`(`idMarca`),
 	FOREIGN KEY(`idTipoVehiculo`) REFERENCES `tipoVehiculos`(`idTipoVehiculo`)
 )
 ENGINE = InnoDB;
 
---------------Se añade columna de lugar de salida--------------
+-- ------------Se añade columna de lugar de salida--------------
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`excursiones`(
 	`idExcursion` INT NOT NULL AUTO_INCREMENT,
 	`idDestino` INT NOT NULL,
-	`fechaSalida` DATE NOT NULL, --FORMATO: YYYY-MM-DD
+	`fechaSalida` DATE NOT NULL, -- FORMATO: YYYY-MM-DD
 	`lugarSalida` VARCHAR(100),
 	`cupos` INT NOT NULL,
-	`cuposDisponibles` INT NOT NULL, --SE QUEDA?--
+	`cuposDisponibles` INT NOT NULL, -- SE QUEDA?--
 	`precio` INT NOT NULL,
 	PRIMARY KEY(`idExcursion`),
 	FOREIGN KEY(`idDestino`) REFERENCES `destinosTuristicos`(`idDestino`)
 )
 ENGINE = InnoDB;
 
-------------------Tabla agregada en actualizacion 2-------------------
+-- ----------------Tabla agregada en actualizacion 2-------------------
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`servicios`(
 	`idServicio` INT NOT NULL AUTO_INCREMENT,
 	`servicio` VARCHAR(100) NOT NULL,
@@ -98,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`servicios`(
 )
 ENGINE = InnoDB;
 
-------------------Tabla agregada en actualizacion 2-------------------
+-- ----------------Tabla agregada en actualizacion 2-------------------
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`serviciosIncluidos`(
 	`idServicioIncluido` INT NOT NULL AUTO_INCREMENT,
 	`idServicio` INT NOT NULL,
@@ -109,21 +110,21 @@ CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`serviciosIncluidos`(
 )
 ENGINE = InnoDB;
 
-------------------PUEDEN SER ELIMINADAS----------------------------
+-- ----------------PUEDEN SER ELIMINADAS----------------------------
 
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`flotillaXexcursion`(
 	`idFxE` INT NOT NULL AUTO_INCREMENT,
 	`idExcursion` INT NOT NULL,
-	`idVehiculo` INT NOT NULL,
+	`idVehiculo` VARCHAR(6) NOT NULL,
 	PRIMARY KEY(`idFxE`),
 	FOREIGN KEY(`idExcursion`) REFERENCES `excursiones`(`idExcursion`),
 	FOREIGN KEY(`idVehiculo`) REFERENCES `flotilla`(`idVehiculo`)
 )
 ENGINE = InnoDB;
 
----TABLA DE INFORMACION DE PAGO???--
+-- -TABLA DE INFORMACION DE PAGO???--
 
--------------------------------------------------------------------
+-- -----------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`reservaciones`(
 	`idReservacion` INT NOT NULL AUTO_INCREMENT,
@@ -132,8 +133,9 @@ CREATE TABLE IF NOT EXISTS `servicioExcursiones`.`reservaciones`(
 	`numeroTelefono` VARCHAR(8) NOT NULL,
 	`camposReservados` INT NOT NULL,
 	`idDestino` INT NOT NULL,
-	`pagado` TINYINT(1) NOT NULL, --PARA SABER SI REALIZÓ EL O NO EL PAGO DE LA RESERVACION
-	`fechaLimite` DATE NOT NULL, --FORMATO YYYY-MM-DD
+	`pagado` TINYINT(1) NOT NULL, -- PARA SABER SI REALIZÓ EL O NO EL PAGO DE LA RESERVACION
+	`fechaLimite` DATE NOT NULL, -- FORMATO YYYY-MM-DD
+	PRIMARY KEY(`idReservacion`)
 )
 ENGINE = InnoDB;
 
