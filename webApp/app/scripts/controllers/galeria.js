5ad853d8f7b67df78caa768fd704b4cc22f27858
karen <klcho.o@hotmail.com>
2018-01-22 02:53:35 -0600

    funcionalidad ADMIN

--- webApp/app/scripts/controllers/galeria.js
+++ webApp/app/scripts/controllers/galeria.js
@@ -8,31 +8,40 @@
  * Controller of the webAppApp
  */
 angular.module('webAppApp')
-    .controller('galeriaCtrl', function ($scope) {
+    .controller('galeriaCtrl', function ($scope,$http,$route) {
         var vm = this;
 
         vm.start = start;
         vm.deletePic = deletePic;
+        vm.addPhoto = addPhoto;
+        vm.cargarImagen = cargarImagen;
 
+        vm.getDestino;
+        vm.pics;
+        vm.localhost = "192.168.0.13";
 
-        vm.pics = ["https://images.unsplash.com/photo-1443890923422-7819ed4101c0?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600",
+     /*  vm.pics = ["https://images.unsplash.com/photo-1443890923422-7819ed4101c0?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600",
             "https://images.unsplash.com/photo-1445964047600-cdbdb873673d?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600",
             "https://images.unsplash.com/photo-1439798060585-62ab242d7724?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600",
             "https://images.unsplash.com/photo-1441906363162-903afd0d3d52?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600",
-            "https://images.unsplash.com/photo-1440339738560-7ea831bf5244?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"];
+            "https://images.unsplash.com/photo-1440339738560-7ea831bf5244?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"];*/
         vm.edit = true;
         vm.btnAcceptCancel = false;
 
         function start() {
             console.log("entro a strart");
-            //............................
-            console.log("entro a strart");
+            $http.get('http://' + vm.localhost + ':3000/getDestinos/').then(function (response) {
+                console.log(response.data);
+                $scope.vm.getDestino = response.data;
+            });
+           
         }
 
         function deletePic(index) {
-            var n = document.getElementById(index);
-            n.remove();
-            /// falta refrescar
+            $http.get('http://' + vm.localhost + ':3000/deleteImagen/'+ index).then(function (response) {
+                console.log(response.data);
+            });
+            vm.cargarImagen();
         }
 
         function archivo(evt) {
@@ -51,7 +60,7 @@ angular.module('webAppApp')
                     return function (e) {
                         // Insertamos la imagen
                         var id = 1;
-                        document.getElementById("columnaPic").innerHTML = ['<div class="imagebox" id="id"><img class="image category-banner" src="', e.target.result, '" title="', escape(theFile.name), '" /><span class="imagebox-desc">holi</span><a ng-click="vm.addPhoto()" type="button" class="btn btn-default btn_upd"><i class="fa fa-upload"></i></a><a ng-click="vm.deletePic()" type="button" class="btn btn-default btn_acept"><i class="fa fa-trash-o" aria-hidden="true"></i></a></a></div>'].join('');
+                        document.getElementById("columnaPic").innerHTML = ['<div class="imagebox" id="id"><img class="image category-banner" src="', e.target.result, '" title="', escape(theFile.name), '" /><a ng-click="'+vm.addPhoto()+'" type="button" class="btn btn-default btn_upd"><i class="fa fa-upload"></i></a></div>'].join('');
                     };
                 })(f);
 
@@ -60,6 +69,26 @@ angular.module('webAppApp')
         }
 
         document.getElementById('files').addEventListener('change', archivo, false);
+
+        // funcion para anadir imagen
+        function addPhoto(){
+            console.log("entra a la funcion");
+            vm.destino = vm.selectDestino.nombreDestino;
+            vm.file= $('input[type=file]').val().replace(/.*(\/|\\)/, '');
+            console.log(vm.file);
+            $http.get('http://' + vm.localhost + ':3000/insertImagenDestino/'+vm.destino+'/'+vm.file).then(function (response) {
+                console.log(response.data);
+            });
+            vm.cargarImagen();
+        }
+
+        function cargarImagen(){
+            vm.destino = vm.selectDestino.nombreDestino;
+            $http.get('http://' + vm.localhost + ':3000/getImagenesDestino/'+vm.destino).then(function (response) {
+                console.log(response.data);
+                vm.pics = response.data;
+            });
+        }
 
 
     });
